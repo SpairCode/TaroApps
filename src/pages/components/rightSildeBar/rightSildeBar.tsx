6fedd6eb9d8d432be7a91ac46f5f7ea2a150f837
SpairCode <1598111569@qq.com>
2019-05-30 19:15:42 +0800

    modifile file

---
+++ src/pages/components/rightSildeBar/rightSildeBar.tsx
@@ -0,0 +1,11 @@
+import Taro, { Component } from '@tarojs/taro'
+// 引入 Swiper, SwiperItem 组件
+import { View } from '@tarojs/components'
+
+export default class rightSildeBar extends Component {
+  render () {
+    return (
+      <View> rightSildeBar </View>
+    )
+  }
+}
--- src/pages/tags/tags.tsx
+++ src/pages/tags/tags.tsx
@@ -1,5 +1,7 @@
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View } from '@tarojs/components'
+import LeftSildeBar from '../components/leftSideBar/leftSildeBar'
+import RightSildeBar from '../components/rightSildeBar/rightSildeBar'
 import './tags.less'
 
 export default class Tags extends Component {
@@ -11,7 +13,12 @@ export default class Tags extends Component {
   render () {
     return (
       <View className='tagBox'>
-
+        <View>
+          <LeftSildeBar></LeftSildeBar>
+        </View>
+        <View>
+          <RightSildeBar></RightSildeBar>
+        </View>
       </View>
     )
   }

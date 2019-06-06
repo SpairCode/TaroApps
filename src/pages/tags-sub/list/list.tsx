48f4a6d4eed6640f0b49797a76032371fb473a51
SpairCode <1598111569@qq.com>
2019-06-06 16:43:39 +0800

    modifile list style

--- src/pages/tags-sub/list/list.tsx
+++ src/pages/tags-sub/list/list.tsx
@@ -1,5 +1,6 @@
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Image } from '@tarojs/components'
+import { View, Image, Text } from '@tarojs/components'
+import './list.scss'
 
 class List extends Component {
   render () {
@@ -9,8 +10,32 @@ class List extends Component {
         { subsList.map((item, index) => {
           return (
             <View className='listItem'>
-              <View>
-                <Image className='itemImage' src={ item.primaryPicUrl } ></Image>  
+              <View className='listItems'>
+                <View>
+                  {/* 商品图片 */}
+                  <View>
+                      <Image className='itemImage' src={ item.primaryPicUrl } ></Image> 
+                  </View>
+                  {/* logo 活动 */}
+                  <View className='logo'>
+                    <Image src={ item.promLogo.logoUrl } ></Image> 
+                  </View>
+                  {/* 限时 */}
+                  <View className='promBanner' style={{ backgroundImage: `url(${ item.promBanner.bannerContentUrl })` }}>
+                    { item.promBanner.content }
+                  </View>
+                  {/* 底部 好货内部价 */}
+                  <View className='promPrice' style={{ backgroundImage: `url(${ item.promBanner.bannerTitleUrl })` }}>
+                    <View className='title'> { item.promBanner.promoTitle } </View>  
+                    <View className='price'> ¥ { item.promBanner.promoSubTitle } </View>
+                  </View>
+                  {/* 底部文字描述 */}  
+                </View>
+              </View>  
+              <View className='textDetails'>
+                 <Text className='title'> { item.name } </Text>
+                 <View> <Text className='activityPrice'> ¥ { item.activityPrice } </Text> <Text className='retailPrice'> ¥ { item.retailPrice } </Text> </View>
+                 <View className='flag'> { item.limitedTag } </View>
               </View>
             </View>
           )
